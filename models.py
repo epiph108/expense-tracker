b00af44dc8c82f8991d937bca69fc6c6951e2d05
Chirag Solanki <solankichiragcs12@gmail.com>
2026-06-01 11:29:13 +0530

    Add - Models file

---
+++ models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, String, Float, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship 
+
+from database import Base
+
+class Expense(Base):
+  __tablename__ = 'expenses'
+  id = Column(Integer, primary_key=True, index=True)
+  title = Column(Float, nullable=False)
+  amount = Column(Float, nullable=False)
+  note = Column(String, nullable=True)
+
+category_id = Column(Integer, ForeignKey('categories.id', nullable=False))
